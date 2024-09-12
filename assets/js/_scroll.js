1e3c5ccaed8f0f29b0342b251f357fe4018db87d
LiquidArtDevelopers <aranaz@gmail.com>
2024-09-12 20:28:29 +0200

    mejoras en clase 4

--- assets/js/_scroll.js
+++ assets/js/_scroll.js
@@ -18,56 +18,54 @@ Este javascript también efectua una animación sobre el NAV el cual debe tener 
 Desarrollado por la magnifica clase del curso de desarrollo web
 */
 
-
-
 //cogemos todos los elementos que tengan clase izquierda y derecha
-const lad_js_IzdEfect = document.getElementsByClassName("lad_izd_efect")
-const lad_js_DerEfect = document.getElementsByClassName("lad_der_efect")
-const lad_js_CenEfect = document.getElementsByClassName("lad_cen_efect")
-
-//cogemos el navegador para el efecto del nav (si existen)
-const navegador = document.getElementById("navegador")
-const subNav01 = document.getElementById("subNav01")
-const toggleLabel = document.getElementById("toggleLabel")
-
+const lad_izd_efect = document.getElementsByClassName("lad_izd_efect")
+const lad_der_efect = document.getElementsByClassName("lad_der_efect")
+const lad_cen_efect = document.getElementsByClassName("lad_cen_efect")
 
 //listener de scroll para modificar el nav y ejecutar animaciones
 // Se ejecuta cuando el usuario hace scroll sobre la ventana
-window.onscroll=function(){ 
-    
-    /* Entro si el tamaño útil del navegador es mayor o igual a 750px */   
+window.onscroll=function(){     
+    // Entro si el tamaño útil del navegador es mayor o igual a 750px   
     if(window.innerWidth>="750"){
-        for(const item of lad_js_IzdEfect){
-            animaciones(item, -5, "1s", true)
+        for(const item of lad_izd_efect){
+            animaciones(item, -5, "2s", true)
         }
-        for(const item of lad_js_DerEfect){
-            animaciones(item, 5, "1s", true)
+        for(const item of lad_der_efect){
+            animaciones(item, 5, "2s", true)
         }        
     }
-    for(const item of lad_js_CenEfect){
+    for(const item of lad_cen_efect){        
         animaciones(item, 0, "2s", true)
-    }
-}
+    } 
+    
+}   
+
+
 
 // Esta función recibe varios parámetros que configurarán la animación a realizar cuando el elemento esté en pantalla. A esta función llegamos cuando se hace scroll.
 function animaciones(el, desplazamiento, duracion, opacidad){
     if (estaEnlaPantalla(el)==true) {
-        el.style.opacity = 1
+        el.style.opacity = "1"
         el.style.transform = "translateX(0)"
         el.style.transform = "translateY(0)"
+        el.style.transition = duracion
     }else{        
         if(opacidad === true){
-            el.style.opacity = 0
+            el.style.opacity = "0"
         }else{
             el.style.opacity = 1
         }
         el.style.transform = `translateX(${desplazamiento}vw)`        
-        el.style.transition = duracion
+        el.style.transition = "0"
     }
 }
 
 /* Función para saber si el elemento recibido está o no en pantalla*/
 function estaEnlaPantalla(el) {
+    console.log("innerheight "+window.innerHeight)
+    console.log("clientheight "+document.documentElement.clientHeight)
     var distance = el.getBoundingClientRect();
+    console.log("Distance del top "+distance.top)
     return (distance.top < (window.innerHeight || document.documentElement.clientHeight) && distance.bottom > 0);
 }
